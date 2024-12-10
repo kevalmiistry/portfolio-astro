b25b9d703d8762c4db2b9de9393bc69860e84d40
kevalmiistry <kevalpm@gmail.com>
2024-12-11 00:43:30 +0530

    REFACT: implementing all icons & link chips with new approach

--- src/components/IconLinkChips/index.js
+++ src/components/IconLinkChips/index.js
@@ -1,8 +1,19 @@
+import Bootstrap from "./Bootstrap.astro"
 import Figma from "./Figma.astro"
+import Puppeteer from "./Puppeteer.astro"
 import Reactjs from "./Reactjs.astro"
+import Shadcn from "./Shadcn.astro"
+import Tailwind from "./Tailwind.astro"
+import Vite from "./Vite.astro"
+
 const icons = {}
 
 icons.reactjs = Reactjs
 icons.figma = Figma
+icons.vite = Vite
+icons.bootstrap = Bootstrap
+icons.tailwind = Tailwind
+icons.shadcn = Shadcn
+icons.puppeteer = Puppeteer
 
 export { icons }
